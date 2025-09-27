18bfb3f509a26b93fca968ac81851548e7765447
Saikiran-435 <saikirankopparthi435@gmail.com>
2025-09-27 13:41:20 +0530

    project Added

---
+++ student-management-system/src/main/java/com/javaspring/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.javaspring.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javaspring.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+          
+}
+
